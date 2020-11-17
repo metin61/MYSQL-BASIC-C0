8644c29e602292edcd1f82f1a42f7dad52d3caae
metin celik <metin.celik@student.rocva.nl>
2020-11-17 12:45:49 +0100

    Delete

--- 03-Delete/taak01-DELETE/antwoorden.sql
+++ 03-Delete/taak01-DELETE/antwoorden.sql
@@ -1,9 +1,14 @@
 -- Opdracht 1 
-
+DELETE FROM players NAME="David Silva";
 -- Opdracht 2 
-
+DELETE FROM players WHERE club="Willem II";
+ 28 rijen bijgewerkt. (Query duurde 0,0310 seconden.)
 -- Opdracht 3
-
+DELETE FROM players WHERE club="FC Barcelona" AND nationality="Belgium";
+ 1 rij bijgewerkt. (Query duurde 0,0304 seconden.)
 -- Opdracht 4
-
+DELETE FROM players WHERE age>=38;	
+ 2 rijen bijgewerkt. (Query duurde 0,0287 seconden.)
 -- Opdracht 5
+DELETE FROM players WHERE id=167905 OR id=16959;
+ 1 rij bijgewerkt. (Query duurde 0,0245 seconden.)
